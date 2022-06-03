2258b39f08f25e0a51ea3ff2b2ed44e4dde819df
Batmend Batsaikhan <batmendbar@Batmends-MacBook-Air.local>
2022-06-03 08:47:46 -0500

    Minimal version where editing item works

--- add-script.js
+++ add-script.js
@@ -1,24 +1,30 @@
 window.$ = window.jQuery = require("jquery");
-
 const fs = require("fs");
+const { isUndefined } = require("util");
+
+let imagePaths = [];
+let image_numbers = 0;
 
 if (!fs.existsSync("items.json")) {
-  fs.writeFileSync("items.json", JSON.stringify([]));
+    fs.writeFileSync("items.json", JSON.stringify([]));
 }
+if (!fs.existsSync("item-under-edit.json")) {
+  fs.writeFileSync("item-under-edit.json", JSON.stringify([]));
+}
+if (!fs.existsSync("items")) {
+  fs.mkdirSync("items");
+}
+
 let items = JSON.parse(fs.readFileSync("items.json"));
-let images = [];
-let categorySelected = false;
+let item_under_edit = JSON.parse(fs.readFileSync("item-under-edit.json"));
 
 $(document).ready(function(){
-  // CATEGORIES
-  $(".category-option").click(function() {
-    $("#categoryDropdown").text($(this).text());
-    categorySelected = true;
-  });
-
-  // IMAGES
+  setInitialValuesForForm()
   $(".select-photo").click(function() {
     $('#file-input').trigger('click');
+  });
+  $(".category-option").click(function() {
+    $("#categoryDropdown").text($(this).text());
   });
   $("#file-input").change(function() {
     if (this.files && this.files[0]) {
@@ -27,23 +33,15 @@ $(document).ready(function(){
       reader.onload = function (e) {
         addNewImage(e.target.result);
       }
-
       reader.readAsDataURL(this.files[0]);
     }
-
   });
-
-  // PUBLISHING
   $(".publish-button").click(function() {
-    if (checkFormCompletion()) {
-      setDefaultDescription();
-      saveImages();
-      saveItemFile();
-      window.location.href = "./admin.html";
-    }
+      if (checkFormCompletion()) {
+          saveItemToJSON();
+          window.location.href = "./admin.html";
+      }
   });
-
-  // BACK
   $(".exit-add-item").click(function() {
     if (confirm("By exiting this page, you may lose the information you have already inputted.")) {
       window.location.href = "./admin.html";
@@ -51,160 +49,139 @@ $(document).ready(function(){
   });
 });
 
-// IMAGES
-function addNewImage(src) {
-  images.push(src);
-  if (images.length == 1) {
-    createCentralImage(src);
-  } else {
-    createBottomImage(src);
-  }
+function addNewImage(src) { //TO DO: add title before picture
+  let filePath = "./items/" + $(".title-input").val() + " (" + image_numbers + ").png";
+  image_numbers++;
+  imagePaths.push(filePath);
+  displayImages();
+  let base64Data = convertImageToBase64(src);
+  fs.writeFileSync(filePath, base64Data, 'base64');
 }
 
-function createCentralImage(image) {
-  $(".central-images").append(
-    `<img class="central-image" src="` + image + `"/>
-    <button class="delete-central-image btn btn-red">
-      <i class="fa fa-trash-o"></i>
-    </button>`
-  );
-  $(".delete-central-image").click(function() {
-    if (confirm("Do you want to delete this image?")) {
-      deleteCentralImage();
-    }
-  });
-  checkImageCompletion();
-}
-
-function createBottomImage(image) {
-  let location = images.length - 1;
-  $(".bottom-images").append(
-    `<img class="bottom-image bottom-image-` + location + `" src="` + image + `"/>`
-  );
-  $(".bottom-image-" + location).click(function() {
-    highlightSecondaryImage(location);
-  });
-}
-
-function checkImageCompletion() {
-  if (existsImage()) {
-    $(".no-image-found").hide();
-    $(".select-photo-text").text("Select another photo");
-  } else {
-    $(".no-image-found").show();
-    $(".select-photo-text").text("Select a photo");
-  }
-}
-
-function highlightSecondaryImage(secondaryIndex) {
-  // Swaps location in JS list
-  let centralImage = images[0];
-  let secondaryImage = images[secondaryIndex];
-  images[0] = secondaryImage;
-  images[secondaryIndex] = centralImage;
-  // Swaps images
-  $(".central-image").attr("src", secondaryImage);
-  $(".bottom-image-" + secondaryIndex).attr("src", centralImage);
-}
-
-function deleteCentralImage() {
-  images.shift();
-  if (images.length == 0) {
-    $(".central-image").remove();
-    $(".delete-central-image").remove();
-  } else {
-    for (let i in images) {
-      images[i].location = i;
-      if (i == 0) {
-        $(".central-image").attr("src", images[i]);
-      } else {
-        $(".bottom-image-" + i).attr("src", images[i]);
-      }
-    }
-    $(".bottom-image-" + images.length).remove();
-  }
-  checkImageCompletion();
-}
-
-// PUBLISH CHECKS
-function checkFormCompletion() {
-  if (!existsTitle()) {
-    alert("Give the item a title before publishing!");
-  } else if (!isTitleValid()) {
-    alert("The current system does not allow special characters in titles. Please only use letters, numbers, underscores, dashes, periods, commas, and spaces.")
-  } else if (!isTitleUnique()) {
-    alert("Another item already has this title. Please enter a unique item title.");
-  } else if (!existsPrice()) {
-    alert("Give the item a price before publishing!");
-  } else if (!categorySelected) {
-    alert("Give the item a category before publishing!");
-  } else if (!existsImage()) {
-    alert("Give the item an image before publishing!");
-  } else {
-    return true;
-  }
-  return false;
-}
-
-function existsTitle() {
-  return ($(".title-input").val() != "");
-}
-
-function existsPrice() {
-  return ($(".price-input").val() != "");
-}
-
-function existsImage() {
-  return (images.length > 0);
-}
-
-// If no description is added, sets a default description
-function setDefaultDescription() {
-  if ($(".description-input").val() == "") {
-    $(".description-input").val("This item has no description.");
-  }
-}
-
-// Shouldn't be necessary, but I wrote my code really quickly so it isn't secure enough
-// To handle certain characters
-function isTitleValid() {
-  let title = $(".title-input").val();
-  let valid_chars = /^[a-zA-Z0-9_,. -]*$/;
-  return valid_chars.test(title);
-}
-
-function isTitleUnique() {
-  for (let item_index in items) {
-    let item = items[item_index];
-    if ($(".title-input").val() == item.title) { return false; }
-  }
-  return true;
-}
-
-function saveImages() {
-  if(!fs.existsSync("items")) {
-    fs.mkdirSync("items");
-  }
-  for (let i in images) {
-    let base64Data = cleanImageData(images[i]);
-    let fileTitle = $(".title-input").val() + " (" + i + ").png";
-    images[i] = "./items/" + fileTitle;
-    fs.writeFileSync("./items/" + fileTitle, base64Data, 'base64');
-  }
-}
-
-function cleanImageData(image) {
+function convertImageToBase64(image) {
   return image.replace(/^data:image\/png;base64,/, "").replace(/^data:image\/jpeg;base64,/, "").replace(/^data:image\/jpg;base64,/, "");
 }
 
-function saveItemFile() {
-  let new_item = {
-    "title": $(".title-input").val(),
-    "description": $(".description-input").val(),
-    "price": $(".price-input").val(),
-    "categories": [$("#categoryDropdown").text()],
-    "images": images
-  };
-  items.push(new_item);
-  fs.writeFileSync("items.json", JSON.stringify(items));
+function displayImages() {
+    $(".central-images").empty();
+    $(".bottom-images").empty();
+    for (i in imagePaths) {
+        if (i == 0) {
+        createCentralImage(imagePaths[i]);
+        }  else {
+        createBottomImage(imagePaths[i]);
+        }
+    }
+    if (imagePaths.length == 0) {
+        $(".select-photo-text").text("Select a photo");
+    } else {
+        $(".select-photo-text").text("Select another photo");
+    }
 }
+
+function createCentralImage(image) {
+    $(".central-images").append(
+    `<img class="central-image" src="` + image + `"/>
+    <button class="delete-central-image btn btn-red">
+        <i class="fa fa-trash-o"></i>
+    </button>`
+    );
+    $(".delete-central-image").click(function() {
+        if (confirm("Do you want to delete this image?")) {
+            deleteCentralImage();
+        }
+    })
+}
+
+function createBottomImage(image) {
+  $(".bottom-images").append(
+    `<img class="bottom-image " src="` + image + `"/>`
+  );
+}
+
+function deleteCentralImage() {
+    imagePaths.shift();
+    displayImages();
+}
+
+function saveItemToJSON() {
+    let new_item = {
+      "title": $(".title-input").val(),
+      "description": $(".description-input").val(),
+      "price": $(".price-input").val(),
+      "categories": [$("#categoryDropdown").text()],
+      "images": imagePaths,
+      "image_numbers": image_numbers
+    };
+    items.push(new_item);
+    fs.writeFileSync("items.json", JSON.stringify(items));
+}
+
+function checkFormCompletion() {
+    if (!existsTitle()) {
+      alert("Give the item a title before publishing!");
+    } else if (!isTitleValid()) {
+      alert("The current system does not allow special characters in titles. Please only use letters, numbers, underscores, dashes, periods, commas, and spaces.")
+    } else if (!isTitleUnique()) {
+      alert("Another item already has this title. Please enter a unique item title.");
+    } else if (!existsPrice()) {
+      alert("Give the item a price before publishing!");
+    // } else if (!categorySelected) {
+    //   alert("Give the item a category before publishing!");
+    } else if (!existsImage()) {
+      alert("Give the item an image before publishing!");
+    } else {
+      setDefaultDescription();
+      return true;
+    }
+    return false;
+}
+  
+function existsTitle() {
+    return ($(".title-input").val() != "");
+}
+  
+function existsPrice() {
+    return ($(".price-input").val() != "");
+}
+  
+function existsImage() {
+    return (imagePaths.length > 0);
+}
+  
+// If no description is added, sets a default description
+function setDefaultDescription() {
+    if ($(".description-input").val() == "") {
+        $(".description-input").val("This item has no description.");
+    }
+}
+  
+  // Shouldn't be necessary, but I wrote my code really quickly so it isn't secure enough
+  // To handle certain characters
+function isTitleValid() {
+    let title = $(".title-input").val();
+    let valid_chars = /^[a-zA-Z0-9_,. -]*$/;
+    return valid_chars.test(title);
+}
+  
+function isTitleUnique() {
+    for (let item_index in items) {
+        let item = items[item_index];
+        if ($(".title-input").val() == item.title) { return false; }
+    }
+    return true;
+}
+
+function setInitialValuesForForm() {
+    $(".title-input").val(item_under_edit.title)
+    $(".description-input").val(item_under_edit.description)
+    $("#categoryDropdown").text(item_under_edit.categories);
+    $(".price-input").val(item_under_edit.price)
+    if (item_under_edit.images === undefined) {
+        imagePaths = [];
+    } else {
+        imagePaths = item_under_edit.images;
+    }
+    image_numbers = item_under_edit.image_numbers; 
+    displayImages();
+  }
--- admin-script.js
+++ admin-script.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 let items = [];
 
 let Item = class {
-  constructor(title, description, price, categories, images) {
+  constructor(title, description, price, categories, images, image_numbers) {
     this.title = title;
     this.description = description;
     this.price = price;
     this.categories = categories;
     this.images = images;
+    this.image_numbers = image_numbers;
     this.id = title.replace(/\s/g, '-');
   }
 }
@@ -20,12 +21,19 @@ function addItemToPage(item) {
       <img class="item-cell-img" src="` + item.images[0] + `" />
       <h4 class="item-cell-title">` + item.title + `</h4>
       <i class="admin-trash-icon item-delete-` + item.id + ` fa fa-trash-o"></i>
+      <i class="admin-edit-icon item-edit-` + item.id + ` fa fa-edit"></i>
     </div>`
   );
   $(".item-delete-" + item.id).click(function() {
     if (confirm("Are you sure you want to remove '" + item.title + "' from the Lighten Up Sale Page? People will no longer be able to purchase this item.")) {
       permanentlyDeleteItem(item);
     }
+  });
+  $(".item-edit-" + item.id).click(function() {
+    permanentlyDeleteItem(item);
+    fs.writeFileSync("item-under-edit.json", JSON.stringify(item));
+    window.location.href = "./add-item.html";
+
   });
 }
 
@@ -64,6 +72,7 @@ $(document).ready(function() {
   }
 
   $("#add-item").click(function () {
+    fs.writeFileSync("item-under-edit.json", JSON.stringify([]));
     window.location.href = "./add-item.html";
   });
 
